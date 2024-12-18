30166192b744a90c524d0b029d7c8e06b4526362
Dnyo666 <dnyo666@foxmail.com>
2024-12-18 22:48:17 +0800

    fix1

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-import { MCServer } from './apps/mc-server.js'
-import { MCPush } from './apps/mc-push.js'
-import { MCAuth } from './apps/mc-auth.js'
-import { helpApp } from './apps/help.js'
+import fs from 'fs'
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 // 输出加载提示
 logger.info('------------------------------------')
@@ -12,11 +14,30 @@ logger.info('QQ群：303104111')
 logger.info('项目地址：https://github.com/Dnyo666/mctool-plugin')
 logger.info('------------------------------------')
 
-const apps = {
-    MCServer,
-    MCPush,
-    MCAuth,
-    help: helpApp
+// 动态加载所有插件
+const files = fs.readdirSync(join(__dirname, 'apps'))
+    .filter(file => file.endsWith('.js'))
+
+let ret = []
+for (let file of files) {
+    ret.push(import(`file://${join(__dirname, 'apps', file)}`))
+}
+
+ret = await Promise.allSettled(ret)
+
+let apps = {}
+for (let i in files) {
+    let name = files[i].replace('.js', '')
+    if (ret[i].status != 'fulfilled') {
+        logger.error(`载入插件错误：${logger.red(name)}`)
+        logger.error(ret[i].reason)
+        continue
+    }
+    const mod = ret[i].value
+    const keys = Object.keys(mod)
+    if (keys.length > 0) {
+        apps[name] = mod[keys[0]]
+    }
 }
 
 export { apps }
