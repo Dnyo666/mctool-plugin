c0ac6831fa1c902afdc5777fe36cf8a16f32a264
Dnyo666 <dnyo666@foxmail.com>
2024-12-18 22:44:40 +0800

    test

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-import plugin from '../../lib/plugins/plugin.js'
-import common from '../../lib/common/common.js'
 import { MCServer } from './apps/mc-server.js'
 import { MCPush } from './apps/mc-push.js'
 import { MCAuth } from './apps/mc-auth.js'
